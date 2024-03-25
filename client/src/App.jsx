b5ec42116ecb44c0c3932dff40e907cf67b92fd7
thejas-1999 <thejask074@gmail.com>
2024-03-25 10:19:20 +0530

    initialize the tailwind in to the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="text-3xl font-bold underline bg-red-900">Hello world!</h1>
     </>
   );
 }
