9f26d65d48a5defe4043941316a65d66f5d6ca90
thejas-1999 <thejask074@gmail.com>
2024-03-25 10:31:06 +0530

    create a navbar for the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-900">Hello world!</h1>
+      <NavBar></NavBar>
     </>
   );
 }
--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,1 +1,21 @@
-
+const NavBar = () => {
+  return (
+    <nav className="bg-purple-200 flex  justify-between items-center">
+      <div className="logo font-bold px-4 h-14 ">Passop</div>
+      <ul>
+        <li className="flex gap-4">
+          <a className="hover font-bold" href="#">
+            Home
+          </a>
+          <a className="hover font-bold" href="#">
+            About
+          </a>
+          <a className="hover font-bold" href="#">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default NavBar;
