3220dc474463cd8e16eb3b0479aae575759b4942
thejas-1999 <thejask074@gmail.com>
2024-03-25 12:26:14 +0530

    create a button for add and add a image for show password

--- client/src/Components/Manager.jsx
+++ client/src/Components/Manager.jsx
@@ -3,7 +3,7 @@ const Manager = () => {
     <>
       <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
-      <div className="  bg-slate-50 myContainer">
+      <div className="  myContainer">
         <h1 className="text-4xl font-bold text-center">
           <span className="text-purple-700">&lt;</span>
           Pass<span className="text-purple-700">OP</span>
@@ -13,22 +13,37 @@ const Manager = () => {
           Your own Password Manager
         </p>
 
-        <div className=" flex flex-col p-4 text-black gap-3">
+        <div className=" flex flex-col p-4 text-black gap-3 items-center">
           <input
             className="rounded-full border border-purple-500 w-full p-4 py-1"
             type=""
+            placeholder="Enter Website URL..."
           />
           <div className="flex w-full justify-between gap-8">
             <input
               className="rounded-full border border-purple-500 w-full p-4 py-1"
               type=""
+              placeholder="Enter Username..."
             />
-            <input
-              className="rounded-full border border-purple-500 w-full p-4 py-1"
-              type=""
-            />
+            <div className="relative">
+              <input
+                className="rounded-full border border-purple-500 w-full p-4 py-1"
+                type=""
+                placeholder="Enter Password..."
+              />
+              <span className="absolute right-[3px] top-[4px] cursor-pointer">
+                <img className="py-1" width={20} src="icons/eye.png" alt="" />
+              </span>
+            </div>
           </div>
-          <button>Add Password</button>
+
+          <button className="flex gap-2 justify-center items-center bg-purple-500 rounded-full px-4 py-2 w-fit hover:bg-purple-300 border-2 border-purple-700">
+            <lord-icon
+              src="https://cdn.lordicon.com/jgnvfzqg.json"
+              trigger="hover"
+            ></lord-icon>
+            Add Password
+          </button>
         </div>
       </div>
     </>
