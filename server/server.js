768fabbfda0d78b15a95e55f2ca839cd12954db5
thejas-1999 <thejask074@gmail.com>
2024-03-30 15:42:51 +0530

    create a basic server with express js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-require("dotenv").config();
+
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-  console.log(`app is runnning on http://localhost:${PORT}`);
+  console.log(`app is running on http://localhost:${PORT}`);
 });
